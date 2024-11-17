3879825b40d0a8d88c532c5931eaf661f78d9406
CHETOUI <92180957+anaschetoui@users.noreply.github.com>
2024-11-17 20:54:42 +0100

    first commit

---
+++ Fill-Array-With-Random-Numbers-From-1-To-100.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+	int Num;
+	do
+	{
+		cout << Message;
+		cin >> Num;
+	} while (Num < 0);
+
+	return Num;
+}
+
+int RandomNumber(int from, int to)
+{
+	return rand() % (to - from + 1) + from;
+}
+
+void FillArray(int arr[100], int& arrLength)
+{
+	for (int i = 0; i < arrLength; i++)
+	{
+		arr[i] = RandomNumber(1, 100);
+	}
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+	for (int i = 0; i < arrLength; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[100], arrLength = ReadPositiveNumber("Please enter a positive number: ");
+
+	FillArray(arr, arrLength);
+
+	cout << "Array Elements: ";
+	PrintArray(arr, arrLength);
+	cout << "\n\n";
+
+	return 0;
+}
